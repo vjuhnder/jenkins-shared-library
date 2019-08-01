4294ac2aad6aa21ee5cda36a218c1c8132efcb52
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-01 15:08:17 +0530

    enabled all results for test

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -4,10 +4,13 @@ def call(String buildResult) {
   if ( buildResult == "FAILURE" ) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
   }
-//  else if( buildResult == "UNSTABLE" ) { 
-//    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
-//  }
-//  else {
-//    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
-//  }
+  else if( buildResult == "SUCCESS" ) { 
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"    
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+  }
 }
