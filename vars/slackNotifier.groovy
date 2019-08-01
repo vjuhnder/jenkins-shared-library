faf4245613ff3ccd09427b4bf9c1ec6a91c04b0d
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-01 15:32:31 +0530

    added url and display name

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
 
-def call(String buildResult) {
+def call(String buildResult, String commitID, String culpritName) {
   if ( buildResult == "FAILURE" ) {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was failed"
   }
   else if( buildResult == "SUCCESS" ) { 
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"    
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was successful"    
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was unstable"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} its result was unclear"	
   }
 }
