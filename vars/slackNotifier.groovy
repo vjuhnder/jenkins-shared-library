b9f1ada551d0a6aafb7bca84e19d60c3f8ea1b51
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-01 18:22:00 +0530

    fixed to getAt

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,17 +1,17 @@
 #!/usr/bin/env groovy
 
 def call(String[] slackData) {
-  if ( slackData.get(0) == "FAILURE" ) {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was failed. Committer: ${slackData.get(1) } "
+  if ( slackData.getAt(0) == "FAILURE" ) {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was failed. Committer: ${slackData.getAt(1) } "
   }
-  else if( slackData.get(0)  == "SUCCESS" ) { 
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was successful. Committer: ${slackData.get(1) } "    
+  else if( slackData.getAt(0)  == "SUCCESS" ) { 
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was successful. Committer: ${slackData.getAt(1) } "    
   }
-  else if( slackData.get(0)  == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was unstable. Committer: ${slackData.get(1) } "
+  else if( slackData.getAt(0)  == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was unstable. Committer: ${slackData.getAt(1) } "
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} its result was unclear. Committer: ${slackData.get(1) } "	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} its result was unclear. Committer: ${slackData.getAt(1) } "	
   }
   
 }
