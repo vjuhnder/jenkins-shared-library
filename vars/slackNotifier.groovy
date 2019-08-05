70e03e850a51a3c992096db909fbdde531891df9
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-05 13:04:26 +0530

    added aborted

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,16 +2,19 @@
 
 def call(String[] slackData) {
   if ( slackData.getAt(0) == "FAILURE" ) {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was failed. Committer: ${slackData.getAt(1) } "
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "
   }
   else if( slackData.getAt(0)  == "SUCCESS" ) { 
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was successful. Committer: ${slackData.getAt(1) } "    
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "    
   }
   else if( slackData.getAt(0)  == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was unstable. Committer: ${slackData.getAt(1) } "
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "
+  }
+  else if( slackData.getAt(0)  == "ABORTED" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} its result was unclear. Committer: ${slackData.getAt(1) } "	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} its result was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "	
   }
   
 }
