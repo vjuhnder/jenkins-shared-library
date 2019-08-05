8366e7314b5d47aee89a70986d8d3e2994263fa2
vjuhnder <52772763+vjuhnder@users.noreply.github.com>
2019-08-05 17:16:58 +0530

    Comment success

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -5,7 +5,7 @@ def call(String[] slackData) {
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "
   }
   else if( slackData.getAt(0)  == "SUCCESS" ) { 
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "    
+    //slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "    
   }
   else if( slackData.getAt(0)  == "UNSTABLE" ) { 
     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_DISPLAY_NAME} ${env.BUILD_URL} was ${slackData.getAt(0)}. Committer: ${slackData.getAt(1) } "
